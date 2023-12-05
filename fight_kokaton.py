b17d140363340958a303bfecea38e1a068ee0ecc
kato yuta <c0b2216770@edu.teu.ac.jp>
2023-12-05 14:36:11 +0900

    練習2 : よろこびエフェクト

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -168,6 +168,7 @@ def main():
 
         if beam is not None and bomb is not None:
             if beam.rct.colliderect(bomb.rct):
+                bird.change_img(6, screen)
                 beam = None
                 bomb = None
 
